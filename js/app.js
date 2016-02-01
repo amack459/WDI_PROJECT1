c320ab13fbe56a9760f0f908b9320fca4c0a75c2
Acacia Mack <acacialmack@gmail.com>
2016-02-01 09:03:55 -0500

    added all column win conditions

--- js/app.js
+++ js/app.js
@@ -57,6 +57,7 @@ function checkSquares(squares) {
   }, true);
 };
 
+
 //checks all squares to make sure there isn't an empty. If there isn't and the checkForWin() doesn't detect a winner. Then, it's a tie
 function checkForTie(){
     var tie = true;
@@ -120,11 +121,51 @@ function checkRows() {
 };
 
 function checkColumns() {
-  return  checkSquares([squares[0],squares[5],squares[10],squares[15],squares[20]]) ||
-          checkSquares([squares[1],squares[6],squares[11],squares[16],squares[21]]) ||
-          checkSquares([squares[2],squares[7],squares[12],squares[17],squares[22]]) ||
-          checkSquares([squares[3],squares[8],squares[13],squares[18],squares[23]]) ||
-          checkSquares([squares[4],squares[9],squares[14],squares[19],squares[24]]);
+  return  checkSquares([squares[0],squares[9],squares[18],squares[27],squares[36]]) ||
+          checkSquares([squares[9],squares[18],squares[27],squares[36],squares[45]]) ||
+          checkSquares([squares[18],squares[27],squares[36],squares[45],squares[54]]) ||
+          checkSquares([squares[27],squares[36],squares[45],squares[54],squares[63]]) ||
+          checkSquares([squares[36],squares[45],squares[54],squares[63],squares[72]]) ||
+          checkSquares([squares[1],squares[10],squares[19],squares[28],squares[37]]) ||
+          checkSquares([squares[10],squares[19],squares[28],squares[37],squares[46]]) ||
+          checkSquares([squares[19],squares[28],squares[37],squares[46],squares[55]]) ||
+          checkSquares([squares[28],squares[37],squares[46],squares[55],squares[64]]) ||
+          checkSquares([squares[37],squares[46],squares[55],squares[64],squares[73]]) ||
+          checkSquares([squares[2],squares[11],squares[20],squares[29],squares[38]]) ||
+          checkSquares([squares[11],squares[20],squares[29],squares[38],squares[47]]) ||
+          checkSquares([squares[20],squares[29],squares[38],squares[47],squares[56]]) ||
+          checkSquares([squares[29],squares[38],squares[47],squares[56],squares[64]]) ||
+          checkSquares([squares[38],squares[47],squares[56],squares[64],squares[73]]) ||
+          checkSquares([squares[3],squares[12],squares[21],squares[30],squares[39]]) ||
+          checkSquares([squares[12],squares[21],squares[30],squares[39],squares[48]]) ||
+          checkSquares([squares[21],squares[30],squares[39],squares[48],squares[57]]) ||
+          checkSquares([squares[30],squares[39],squares[48],squares[57],squares[66]]) ||
+          checkSquares([squares[39],squares[48],squares[57],squares[66],squares[75]]) ||
+          checkSquares([squares[4],squares[13],squares[22],squares[31],squares[40]]) ||
+          checkSquares([squares[13],squares[22],squares[31],squares[40],squares[49]]) ||
+          checkSquares([squares[22],squares[31],squares[40],squares[49],squares[58]]) ||
+          checkSquares([squares[31],squares[40],squares[49],squares[58],squares[67]]) ||
+          checkSquares([squares[40],squares[49],squares[58],squares[67],squares[76]]) ||
+          checkSquares([squares[5],squares[14],squares[23],squares[32],squares[41]]) ||
+          checkSquares([squares[14],squares[23],squares[32],squares[41],squares[50]]) ||
+          checkSquares([squares[23],squares[32],squares[41],squares[50],squares[59]]) ||
+          checkSquares([squares[32],squares[41],squares[50],squares[59],squares[68]]) ||
+          checkSquares([squares[41],squares[50],squares[59],squares[68],squares[77]]) ||
+          checkSquares([squares[6],squares[15],squares[24],squares[33],squares[42]]) ||
+          checkSquares([squares[15],squares[24],squares[33],squares[42],squares[51]]) ||
+          checkSquares([squares[24],squares[33],squares[42],squares[51],squares[60]]) ||
+          checkSquares([squares[33],squares[42],squares[51],squares[60],squares[69]]) ||
+          checkSquares([squares[42],squares[51],squares[60],squares[69],squares[78]]) ||
+          checkSquares([squares[7],squares[16],squares[25],squares[34],squares[43]]) ||
+          checkSquares([squares[16],squares[25],squares[34],squares[43],squares[52]]) ||
+          checkSquares([squares[25],squares[34],squares[43],squares[52],squares[61]]) ||
+          checkSquares([squares[34],squares[43],squares[52],squares[61],squares[70]]) ||
+          checkSquares([squares[43],squares[52],squares[61],squares[70],squares[79]]) ||
+          checkSquares([squares[8],squares[17],squares[26],squares[35],squares[44]]) ||
+          checkSquares([squares[17],squares[26],squares[35],squares[44],squares[53]]) ||
+          checkSquares([squares[26],squares[35],squares[44],squares[53],squares[62]]) ||
+          checkSquares([squares[35],squares[44],squares[53],squares[62],squares[71]]) ||
+          checkSquares([squares[44],squares[53],squares[62],squares[71],squares[80]]);
 };
 
 function checkDiags() {
